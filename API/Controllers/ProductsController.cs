aac78b683d179e67626e1dab56e0bb34e5943f4f
vivek-baliyan <vivekbr001@gmail.com>
2024-03-27 00:20:21 +0530

    end of section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+
+    }
+}
